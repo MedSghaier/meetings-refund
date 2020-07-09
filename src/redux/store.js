efb2ea257797a93d689e683ff2207b13784d6fe1
mohamed <mohamed@intuitiv-interactive.com>
2020-07-09 18:34:56 +0100

    added prestited storage to meeting elements

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-
 import rootReducer from './root-reducer';
+import { loadState, saveState } from './localState';
 
 const middlewares = [];
 
-// if (process.env.NODE_ENV === 'development') {
+const persistedState = loadState();
+if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
-// }
+}
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(rootReducer, persistedState, applyMiddleware(...middlewares),);
+
+store.subscribe(()=>{
+  saveState({meetings: store.getState().meetings})
+})
 
 export default store;
