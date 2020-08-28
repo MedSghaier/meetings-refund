78cd8ea7ff62cd22d943a5e98d4cddf6edf80090
mohamed <mohamed@intuitiv-interactive.com>
2020-08-28 11:51:12 +0100

    Updates

---
+++ precache-manifest.5fa4cca1db434e438f0699a62111cdcb.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ed5ac89ca29798803965f9b219fc4b",
+    "url": "/meetings-refund/index.html"
+  },
+  {
+    "revision": "b82baf0961ca9f0d6e0b",
+    "url": "/meetings-refund/static/css/main.d27c4fd8.css"
+  },
+  {
+    "revision": "b82baf0961ca9f0d6e0b",
+    "url": "/meetings-refund/static/js/main.js"
+  }
+]);
